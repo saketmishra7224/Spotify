352627699984ef8b6183c3d8e6dc322783c1d670
Saket <Saket_mishra@gmail.com>
2025-01-23 18:45:52 +0530

    Updated API

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getsongs(folder) {
     currFolder = folder;
-    let a = await fetch(`http://127.0.0.1:3000/${folder}/`)
+    let a = await fetch(`http://10.1.170.55:3001/${folder}/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
